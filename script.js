15028687f0615a2cbc637823df8f83988f0b2697
KOUROUMA Saibou <saiboukourouma12@gmail.com>
2026-05-13 19:09:52 +0200

    Correction Kobo API

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
-const SERVER_URL = "https://kf.kobotoolbox.org";
-const API_KEY = "VOTRE_API_KEY";
-const ASSET_UID = "VOTRE_UID";
+const FORM_URL = "https://kf.kobotoolbox.org/api/v2/assets/aMfrC6SQo5rdpVJfyBwgcC/data/?format=json";
 
 async function chargerDonnees() {
 
   try {
 
-    const response = await fetch(
-      `${SERVER_URL}/api/v2/assets/${ASSET_UID}/data/?format=json`,
-      {
-        headers: {
-          "Authorization": `Token ${API_KEY}`,
-          "Accept": "application/json"
-        }
-      }
-    );
+    const response = await fetch(FORM_URL);
+
+    console.log(response);
 
     const data = await response.json();
 
-    const results = data.results;
+    console.log(data);
+
+    const results = data.results || [];
 
     document.getElementById("total").innerText = results.length;
 
     const tbody = document.getElementById("tbody");
 
+    tbody.innerHTML = "";
+
     results.forEach(item => {
 
-      const tr = document.createElement("tr");
-
-      tr.innerHTML = `
-        <td>${item._id}</td>
-        <td>${item.nom || ""}</td>
-        <td>${item.prenom || ""}</td>
+      tbody.innerHTML += `
+        <tr>
+          <td>${item.Id || ""}</td>
+          <td>${item.Nom || ""}</td>
+          <td>${item.Pr_nom || ""}</td>
+        </tr>
       `;
-
-      tbody.appendChild(tr);
 
     });
 
   } catch(error) {
 
     console.error(error);
+
+    alert(error.message);
 
   }
 
